fe71613a36451a92ed8c5386c9dd8486139de233
Stephanie Syed <stephaniemsyed@utexas.edu>
2022-08-21 13:56:08 -0500

    Rename project

--- stephaniemavisblog/blog.py
+++ stephaniemavisblog/blog.py
@@ -1,8 +1,8 @@
 from flask import Blueprint, flash, g, redirect, render_template, request, url_for
 from werkzeug.exceptions import abort
 
-from flaskr.auth import login_required
-from flaskr.db import get_db
+from stephaniemavisblog.auth import login_required
+from stephaniemavisblog.db import get_db
 
 bp = Blueprint("blog", __name__)
 
